e774880452557c04c1d3f6b3ad701c4e6e22b55e
Martin Ring <martin.ring@dfki.de>
2017-02-02 08:15:01 +0100

    wip

--- src/main/scala/de/dfki/cps/specific/sysml/Test.scala
+++ src/main/scala/de/dfki/cps/specific/sysml/Test.scala
@@ -5,11 +5,9 @@ import java.util
 
 import de.dfki.cps.specific.SysML
 import org.eclipse.emf.common.util.URI
-import org.eclipse.emf.ecore.resource.impl.{ExtensibleURIConverterImpl, ResourceSetImpl}
-import specific.sysml.parser.{IndentScanner, SysMLLexer, SysMLParsers}
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+import org.eclipse.uml2.uml.{Model, UMLFactory}
 
-import scala.io.Source
-import scala.util.parsing.input.Reader
 import scala.collection.JavaConverters._
 
 /**
@@ -18,13 +16,40 @@ import scala.collection.JavaConverters._
 object Test extends App {
   implicit val rs = new ResourceSetImpl
   Synthesis.prepareLibrary(rs)
-  val resources = Seq(
-    "index"
-  )
-  resources.foreach { name =>
-    val uri = URI.createFileURI(s"example/$name.sysml")
-    val resource = rs.createResource(uri.appendFileExtension("uml"))
+
+  println("loading project")
+  val projectURI = URI.createFileURI(s"example/index.sysml")
+  val project = SysML.loadProject(new File(projectURI.toFileString))
+
+  val resource = rs.createResource(projectURI.appendFileExtension("uml"))
+
+  project.includes.foreach { include =>
+    println(s"loading $include")
+    val uri = projectURI.trimSegments(1).appendSegment(include)
     val positions = SysML.load(new File(uri.toFileString),resource)
-    resource.save(new util.HashMap)
   }
+
+  project.mappings.foreach { mapping =>
+    val Seq(modelName, rest @_*) = mapping.supplier.parts
+    val model = resource.getContents.asScala.collectFirst {
+      case m: Model if m.getName == modelName => m
+    }
+    if (rest.length == 0) { // model <|- model mapping
+      val name = mapping.client.content.trim
+      val clientModel = resource.getContents.asScala.collectFirst {
+        case m: Model if m.getName == name => m
+      }
+      for {
+        supplier <- model
+        client <- clientModel
+      } {
+        val realization = UMLFactory.eINSTANCE.createRealization()
+        realization.getSuppliers.add(supplier)
+        realization.getClients.add(client)
+        resource.getContents.add(realization)
+      }
+    }
+  }
+
+  resource.save(new util.HashMap)
 }
