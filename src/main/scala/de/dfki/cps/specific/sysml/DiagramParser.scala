615c4170f1f1cff9a8d260c16ca8c0b70f5cb6b3
Martin Ring <martin.ring@dfki.de>
2019-05-13 12:27:38 +0200

    initial json generation

--- src/main/scala/de/dfki/cps/specific/sysml/DiagramParser.scala
+++ src/main/scala/de/dfki/cps/specific/sysml/DiagramParser.scala
@@ -6,25 +6,47 @@ import org.eclipse.emf.ecore.resource.Resource
 import specific.sysml.parser.{IndentScanner, SysMLLexer, SysMLParsers}
 
 import scala.io.Source
+import spray.json._
+import DefaultJsonProtocol._
+
 
 object DiagramParser extends App {
-  def load(source: File, target: Resource, includeOCL: Boolean = false): Unit = {
+  def convert(element: NamedElement): JsValue = element match {
+    case Diagram(kind,m,n,name,members) =>
+      Map(
+        "name" -> name.toJson,
+        "kind" -> kind.toString.toJson,
+        "members" -> members.map(convert).toJson
+      ).toJson
+    case Block(name,compartments,comments) =>
+      Map(
+        "name" -> name.toJson,
+        "compartments" -> compartments.map(c => convert(c)).toJson
+      ).toJson
+    case default => default.name.toJson
+  }
+
+  def convert(compartment: BlockCompartment): JsValue = compartment match {
+    case OperationsCompartment(ops) =>
+      Map(
+        "name" -> "operations".toJson,
+        "items" -> ops.map(convert).toJson
+      ).toJson
+    case default => default.compartmentName.toJson
+  }
+
+  def load(source: File): Unit = {
     val textSource = Source.fromFile(source)
     val tokens = new IndentScanner(new SysMLLexer.Scanner(textSource.mkString))
 
     SysMLParsers.phrase(SysMLParsers.diagram)(tokens) match {
       case SysMLParsers.Success(b: Diagram,_) =>
-        val synth = new Synthesis(source.toString,target)
-        synth.includeOCL = includeOCL
-        synth.structure(b)
-        synth.naming(b)
-        synth.parseConstraints(b)
-        synth.positions.toMap
+        println(convert(b).prettyPrint)
       case SysMLParsers.NoSuccess(msg,i) =>
-        target.getErrors.add(SemanticMessage(source.toString,i.pos,msg,None))
-        Map.empty
-    }
+        println(s"parse error: $msg")
 
-    // tokens.toJson.prettyPrint  // Das ist leider zu einfach und falsch
+    }
   }
+
+  load(new File("example/02-fsl0.sysml"))
 }
